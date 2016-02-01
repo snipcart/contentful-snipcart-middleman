d4f698b68730d87a77d6f89dd27e013ed0f0e033
yanickouellet <yanickouellet555@gmail.com>
2016-01-31 20:03:43 -0500

    Use new syntax for ruby hash

--- config.rb
+++ config.rb
@@ -49,20 +49,20 @@ end
 
 data.contentful.product.each do |elem|
   p = elem[1]
-  proxy "products/#{p.slug}.html", "product.html", :locals => { :product => p }, :ignore => true
+  proxy "products/#{p.slug}.html", "product.html", locals: { product: p }, ignore: true
 end
 
 helpers do
   def snipcart_button (p, text)
     args = {
-            :"class" => "snipcart-add-item",
-            :"data-item-id" => p.id,
-            :"data-item-price" => p.price,
-            :"data-item-name" => p.name,
-            :"data-item-url" => current_page.url,
-            :"data-item-max-quantity" => p.max_quantity,
-            :"data-item-description" => p.description,
-            :"data-item-image" => "http:#{p.image.url}"
+            "class":"snipcart-add-item",
+            "data-item-id": p.id,
+            "data-item-price": p.price,
+            "data-item-name": p.name,
+            "data-item-url": current_page.url,
+            "data-item-max-quantity": p.max_quantity,
+            "data-item-description": p.description,
+            "data-item-image": "http:#{p.image.url}"
     }
 
     content_tag :button, args do
